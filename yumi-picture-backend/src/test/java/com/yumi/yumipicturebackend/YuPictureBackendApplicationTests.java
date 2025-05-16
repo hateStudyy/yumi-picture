7be1607d788f2b2cdd399a002b67c13bd4264cbe
yumi <835094257@qq.com>
2025-05-16 14:43:24 +0800

    后端项目优化

---
+++ yumi-picture-backend/src/test/java/com/yumi/yumipicturebackend/YuPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yumi.yumipicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumiPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
