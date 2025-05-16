79176413f3076bc11feb131e9edd16eadeb577dc
yumi <835094257@qq.com>
2025-05-16 14:23:31 +0800

    新增测试接口代码，功能完善

--- yu-picture-backend/src/main/java/com/yupi/yupicturebackend/controller/MainController.java
+++ yu-picture-backend/src/main/java/com/yupi/yupicturebackend/controller/MainController.java
@@ -2,12 +2,18 @@ package com.yupi.yupicturebackend.controller;
 
 import com.yupi.yupicturebackend.common.BaseResponse;
 import com.yupi.yupicturebackend.common.ResultUtils;
+import com.yupi.yupicturebackend.exception.BusinessException;
+import com.yupi.yupicturebackend.exception.ErrorCode;
+import com.yupi.yupicturebackend.exception.ThrowUtils;
+import com.yupi.yupicturebackend.model.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class MainController {
 
     /**
@@ -15,6 +21,28 @@ public class MainController {
      */
     @GetMapping("/health")
     public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+
+    // todo delete
+    /**
+     * throwIfTest
+     */
+    @GetMapping("/throwIfTest")
+    public BaseResponse<String> throwIfTest() {
+        User user = null;
+
+        try {
+            ThrowUtils.throwIf(user == null, new BusinessException(ErrorCode.SYSTEM_ERROR,"异常报错"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
+        System.out.println("执行！！！");
+        System.out.println("执行！！！");
+        System.out.println("执行！！！");
+        System.out.println("执行！！！");
+        System.out.println("执行！！！");
         return ResultUtils.success("ok");
     }
 
--- yu-picture-backend/src/main/java/com/yupi/yupicturebackend/manager/FileManager.java
+++ yu-picture-backend/src/main/java/com/yupi/yupicturebackend/manager/FileManager.java
@@ -15,6 +15,7 @@ import com.yupi.yupicturebackend.exception.ThrowUtils;
 import com.yupi.yupicturebackend.model.dto.file.UploadPictureResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
@@ -98,9 +99,12 @@ public class FileManager {
         // 1. 校验文件大小
         long fileSize = multipartFile.getSize();
         final long ONE_M = 1024 * 1024;
-        ThrowUtils.throwIf(fileSize > 2 * ONE_M, ErrorCode.PARAMS_ERROR, "文件大小不能超过 2MB");
+        ThrowUtils.throwIf(fileSize > 100 * ONE_M, ErrorCode.PARAMS_ERROR, "文件大小不能超过 100MB");
         // 2. 校验文件后缀
         String fileSuffix = FileUtil.getSuffix(multipartFile.getOriginalFilename());
+        if(fileSuffix != null && !fileSuffix.isEmpty()){
+            fileSuffix = fileSuffix.toLowerCase();
+        }
         // 允许上传的文件后缀列表（或者集合）
         final List<String> ALLOW_FORMAT_LIST = Arrays.asList("jpeg", "png", "jpg", "webp");
         ThrowUtils.throwIf(!ALLOW_FORMAT_LIST.contains(fileSuffix), ErrorCode.PARAMS_ERROR, "文件类型错误");
